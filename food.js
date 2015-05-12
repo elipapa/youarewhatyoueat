184dedd1106f129ebc8fafaea91ae150e486b2bd
Eli Papa <elipapa@lapofeli2.home>
2015-05-13 00:07:44 +0100

    first dc.js charts working

---
+++ food.js
@@ -0,0 +1,102 @@
+'use strict';
+
+/* jshint globalstrict: true */
+/* global dc,d3,crossfilter,colorbrewer */
+
+    
+var categoryChart = dc.rowChart('#category-chart');
+var mealChart = dc.pieChart('#meal-chart');
+var timechart = dc.barChart("#time-chart");
+var mealList = dc.dataTable("#dc-data-table");
+	
+d3.csv('fooddata_plot.json', function (data) {
+    /* since its a csv file we need to format the data a bit */
+    var dateFormat = d3.time.format('%Y%m%d');
+
+    data.forEach(function (d) {
+        d.dd = dateFormat.parse(d.date);
+        d.week = d3.time.week(d.dd); // pre-calculate week for better performance
+        d.calories = +d.calories; // coerce to number
+        d.sugars_g = +d.sugars_g;
+    });
+
+    var ndx = crossfilter(data);
+    
+    var dateDim = ndx.dimension(function (d) { return d.dd;  });
+    var weekDim = ndx.dimension(function (d) { return d.week; });
+    var categoryDim = ndx.dimension( function (d) { return d.category; });
+    var mealDim = ndx.dimension( function (d) { return d.meal;});
+    
+    var categoryGroup = categoryDim.group();
+    var mealGroup = mealDim.group();
+
+    var all = ndx.groupAll();
+    var calByDate = dateDim.group().reduceSum(function (d) {
+    	return d.calories;
+    });
+
+
+	mealChart.width(400)
+	        .height(400)
+	        .radius(180)
+	        .innerRadius(40)
+	        .dimension(mealDim)
+	        .group(mealGroup);
+
+	categoryChart.width(400)
+	        .height(400)
+	        .margins({top: 20, left: 10, right: 10, bottom: 20})
+	        .group(categoryGroup)
+	        .dimension(categoryDim)
+	        // assign colors to each value in the x scale domain
+	        // title sets the row text
+	        .renderLabel(true)
+	        .renderTitle(true)
+	        .elasticX(true)
+	        .xAxis().ticks(4);
+
+	timechart
+	    .width(600)
+	    .height(160)
+	    .margins({top: 10, right: 50, bottom: 30, left: 50})
+	    .dimension(dateDim)
+	    .group(calByDate)
+	    .transitionDuration(500)
+	    .x(d3.time.scale().domain([new Date(2014, 9, 18), new Date(2015, 3, 19)]))
+	    .elasticY(true)
+	    .xAxisLabel("Date")
+	    .yAxis().ticks(4);
+
+	mealList
+        .dimension(dateDim)
+        .group(function (d) {
+            var format = d3.format('02d');
+            return d.dd.getFullYear() + '/' + format((d.dd.getMonth() + 1));
+        })
+        .size(10) // (optional) max number of records to be shown, :default = 25
+        // There are several ways to specify the columns; see the data-table documentation.
+        // This code demonstrates generating the column header automatically based on the columns.
+        .columns([
+        function(d) {
+            return d.date;
+        },
+        function(d) {
+            return d.meal;
+        },
+        function(d) {
+            return d.fooditem;
+        },
+        function(d) {
+            return d.calories;
+        },
+        function(d) {
+            return d.category;
+        }
+    	])
+    	.renderlet(function (table) {
+            table.selectAll('.dc-table-group').classed('info', true);
+        });
+
+	dc.renderAll();
+
+});
